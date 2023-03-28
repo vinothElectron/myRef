82cbc2a62aaf1fc743933d845c1fcb1238ac6ce4
vinothElectron <124486803+vinothElectron@users.noreply.github.com>
2023-03-28 08:26:10 +0530

    added base content to imprt kv file

--- Vault_restore/Import_vault_kv.sh
+++ Vault_restore/Import_vault_kv.sh
@@ -1,1 +1,22 @@
+#!/bin/bash
+
+json_file=$1
+secret_engine=$2
+path=""
+epath=""
+
+
+jq . $json_file >/dev/null
+if [ $? != 0 ];
+then
+    error "invalid json"
+    exit 0
+fi
+out=`jq -c . $json_file`
+if [ $out == "" ]
+then
+   error "empty file"
+    exit 0
+fi
+echo "started"
 
