c6ea66b5fc87fdd618266c4a3f8b9f99b33578e3
vinothElectron <124486803+vinothElectron@users.noreply.github.com>
2023-03-28 08:39:49 +0530

    modify import vault kv file(#4)
    * create import vault kv file
    
    * added base content to imprt kv file
    
    * update import function on import kv file
    
    * add main script to import kv

--- Vault_restore/Import_vault_kv.sh
+++ Vault_restore/Import_vault_kv.sh
@@ -20,3 +20,48 @@ then
 fi
 echo "started"
 
+import(){
+        local base_path=$1
+        local current_path=$2
+        local total_path=""
+
+        #echo "base =$base_path   current=$current_path"
+        if [[ -z "$base_path" || -z "$current_path" ]];then
+                total_path=$base_path$current_path
+        else
+                total_path=$base_path.$current_path
+        fi
+        #echo "total==$total_path"
+        jq  '.'$total_path' | keys[]' $json_file 2>&1 >/dev/null
+        if [ $? != 0 ];
+        then
+                value=`jq  '.'$base_path'' $json_file`
+                echo  "$base_path ==> $value"
+                array_value=(`echo $value|jq '.|keys[]'`)
+                for key in "${array_value[@]}";do
+                        ivalue=`echo $value|jq '.'$key |sed 's|\"||g'`
+                        ikey=`echo $key|sed 's|\"||g'`
+                        put_path=`echo $base_path|sed 's|"."|/|g;s|\"||g'`
+                        #vault kv put $put_path $ikey=$ivalue
+                        echo "$put_path -->$ikey=$ivalue"
+                done
+
+        else
+                #echo "inner:$base_path"
+                jq -c '.'$total_path' |keys[]' $json_file  |while read i;do
+                        import $total_path $i
+                done
+
+        fi
+
+}
+
+if [ -z $secret_engine ];
+then
+        echo "Please enter second parameter==> root path/KV secret engine name (e.g hp2b)"
+else
+        #vault secrets move secret/ $secret_engine/
+        echo "started"
+        import $path $epath
+        echo "Completed"
+fi
