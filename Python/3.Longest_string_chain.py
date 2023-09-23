cea165177ad555f21976150ec01ff2d7f890eca1
vinothElectron <124486803+vinothElectron@users.noreply.github.com>
2023-09-23 17:28:46 +0000

    Update 3.Longest_string_chain.py

--- Python/3.Longest_string_chain.py
+++ Python/3.Longest_string_chain.py
@@ -1,19 +1,28 @@
-class Solution(object):
-    def longestStrChain(self, words):
-        """
-        :type words: List[str]
-        :rtype: int
-        """
-        if len(words)==1: return 1
-        chain={}
-        words=sorted(words,key=len)
-        for i in range(len(words)):
-            maxi=-1
-            for j in range(len(words[i])):
-                temp=1
-                if words[i][:j]+words[i][j+1:] in chain.keys():
-                   temp+=chain[words[i][:j]+words[i][j+1:]]
-                if maxi<temp:
-                    maxi=temp
-            chain[words[i]]=maxi
-        return max(chain.values())
+class Solution:
+    def longestStrChain(self, words) -> int:
+        # Sort the words by their lengths
+        words.sort(key=len)
+
+        # Dictionary to store the longest chain length for each word
+        longest_chain_length = {}
+
+        # Initialize the answer
+        max_chain_length = -1
+
+        for word in words:
+            # Initialize the chain length for the current word
+            longest_chain_length[word] = 1
+
+            # Try removing one character at a time from the word and check if the resulting word exists
+            for i in range(len(word)):
+                reduced_word = word[:i] + word[i + 1:]
+
+                # If the reduced word exists in the dictionary
+                if reduced_word in longest_chain_length:
+                    # Update the chain length for the current word
+                    longest_chain_length[word] = max(longest_chain_length[word], longest_chain_length[reduced_word] + 1)
+
+            # Update the maximum chain length seen so far
+            max_chain_length = max(max_chain_length, longest_chain_length[word])
+
+        return max_chain_length
